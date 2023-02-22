e35b706e2e6daee4294f54c2336cd9afa11e8a9a
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-02-22 23:04:47 +0100

    form OK

--- src/App.js
+++ src/App.js
@@ -1,70 +1,100 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // components
 import InputProduction from "./components/InputProduction";
 import InputConsumption from "./components/InputConsumption";
-import { Button } from "react-bootstrap";
 
 // style
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [production1, setProduction1] = useState(0);
-  const [production2, setProduction2] = useState(0);
-  const [consumption1, setConsumption1] = useState(0);
-  const [consumption2, setConsumption2] = useState(0);
-  const [consumption3, setConsumption3] = useState(0);
-  const [consumption4, setConsumption4] = useState(0);
-  const [consumption5, setConsumption5] = useState(0);
+  const [production1, setProduction1] = useState("");
+  const [production2, setProduction2] = useState("");
+  const [rate1, setRate1] = useState("");
+  const [rate2, setRate2] = useState("");
+  const [rate3, setRate3] = useState("");
+  const [rate4, setRate4] = useState("");
+  const [rate5, setRate5] = useState("");
+  let totalProduction = Number(production1) + Number(production2);
+
+  // useEffect
+  useEffect(() => {
+    if (
+      Number(rate1) +
+        Number(rate2) +
+        Number(rate3) +
+        Number(rate4) +
+        Number(rate5) >
+      100
+    ) {
+      alert(
+        "La somme des pourcentage fait plus de 100% ! \nModifiez une valeur !!"
+      );
+    }
+  }, [rate1, rate2, rate3, rate4, rate5]);
+
   return (
     <div className="App container">
-      <form className="input-form">
-        <div className="inputs-data">
-          <section className="inputs-production">
-            <InputProduction
-              title={"Producteur n°1"}
-              production={production1}
-              setProduction={setProduction1}
-            />
-            <InputProduction
-              title={"Producteur n°2"}
-              production={production2}
-              setProduction={setProduction2}
-            />
-          </section>
-          <section className="inputs-consumption">
-            <InputConsumption
-              title={"Consommateur n°1"}
-              consumption={consumption1}
-              setConsumption={setConsumption1}
-            />
-            <InputConsumption
-              title={"Consommateur n°2"}
-              consumption={consumption2}
-              setConsumption={setConsumption2}
-            />
-            <InputConsumption
-              title={"Consommateur n°3"}
-              consumption={consumption3}
-              setConsumption={setConsumption3}
-            />
-            <InputConsumption
-              title={"Consommateur n°4"}
-              consumption={consumption4}
-              setConsumption={setConsumption4}
-            />
-            <InputConsumption
-              title={"Consommateur n°5"}
-              consumption={consumption5}
-              setConsumption={setConsumption5}
-            />
-          </section>
-        </div>
-        <Button variant="primary" type="submit">
-          Calculer la répartition
-        </Button>
-      </form>
+      <div className="empty"></div>
+      <div className="input-form">
+        <section className="inputs-production">
+          <h1>Producteurs</h1>
+          <InputProduction
+            title={"Producteur n°1"}
+            production={production1}
+            setProduction={setProduction1}
+          />
+          <InputProduction
+            title={"Producteur n°2"}
+            production={production2}
+            setProduction={setProduction2}
+          />
+          <p>Total Production = {totalProduction.toString()} kWh</p>
+        </section>
+        <section className="inputs-consumption">
+          <h1>Consommateurs</h1>
+          <InputConsumption
+            title={"Consommateur n°1"}
+            rate={rate1}
+            setRate={setRate1}
+            totalProduction={totalProduction}
+          />
+          <InputConsumption
+            title={"Consommateur n°2"}
+            rate={rate2}
+            setRate={setRate2}
+            totalProduction={totalProduction}
+          />
+          <InputConsumption
+            title={"Consommateur n°3"}
+            rate={rate3}
+            setRate={setRate3}
+            totalProduction={totalProduction}
+          />
+          <InputConsumption
+            title={"Consommateur n°4"}
+            rate={rate4}
+            setRate={setRate4}
+            totalProduction={totalProduction}
+          />
+          <InputConsumption
+            title={"Consommateur n°5"}
+            rate={rate5}
+            setRate={setRate5}
+            totalProduction={totalProduction}
+          />
+          <p>
+            Pourcentage de production affecté :{" "}
+            {Number(rate1) +
+              Number(rate2) +
+              Number(rate3) +
+              Number(rate4) +
+              Number(rate5)}{" "}
+            %
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/InputConsumption.js
+++ src/components/InputConsumption.js
@@ -1,17 +1,68 @@
+import { useState } from "react";
+
 import Form from "react-bootstrap/Form";
 
-const InputConsumption = ({ consumption, setConsumption, title }) => {
+// style
+import "../style/consumption.css";
+
+const InputConsumption = ({
+  // consumption,
+  // setConsumption,
+  title,
+  rate,
+  setRate,
+  totalProduction,
+}) => {
+  // init
+  const [consumption, setConsumption] = useState("");
+  let consumptionENR = Math.min(
+    Number(consumption),
+    (Number(totalProduction) * Number(rate)) / 100
+  );
+  let consumptionFossil = consumption - consumptionENR;
+
+  //Return
   return (
-    <Form>
+    <Form className="consumer">
+      <Form.Label className="consumer-title">{title}</Form.Label>
       <Form.Group className="mb-3">
-        <Form.Label>{title} (kWh)</Form.Label>
+        <Form.Label>Consommation totale</Form.Label>
         <Form.Control
           type="number"
-          placeholder="entrez la consommation en kWh"
+          placeholder="kWh"
           value={consumption}
           onChange={(event) => {
             setConsumption(event.target.value);
           }}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Part de Production ENR</Form.Label>
+        <Form.Control
+          type="number"
+          placeholder="%"
+          value={rate}
+          onChange={(event) => {
+            if (Number(event.target.value) <= 100) {
+              setRate(event.target.value);
+            }
+          }}
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Consommation ENR</Form.Label>
+        <Form.Control
+          type="number"
+          value={consumptionENR}
+          disabled
+        ></Form.Control>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Consommation Réseau</Form.Label>
+        <Form.Control
+          type="number"
+          value={consumptionFossil}
+          disabled
         ></Form.Control>
       </Form.Group>
     </Form>
